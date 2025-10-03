a7e91ec705b455d6ae15cefc989281afc5bbb98b
SaltireSoul <rinld0jsg@mozmail.com>
2025-10-03 21:32:38 +0100

    try again

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,14 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(windows, windows_subsystem = "windows")]
 
-use tauri::{Manager, Builder};
+use tauri::{Builder, WindowUrl};
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
   let url = args.get(1).unwrap_or(&"https://copilot.microsoft.com/".to_string());
 
+  let redirect_url = format!("index.html?url={}", url);
+
   Builder::default()
-    .setup(move |app| {
-      let window = app.get_window("main").unwrap();
-      window.eval(&format!("window.location.replace('{}')", url)).unwrap();
-      Ok(())
-    })
-    .run(tauri::generate_context!())
+    .run(tauri::generate_context!().with_url(WindowUrl::App(redirect_url.into())))
     .expect("error while running tauri application");
 }
