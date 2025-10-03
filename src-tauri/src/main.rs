3557df4d5564ab10197f238fb061fad633dc7dc5
SaltireSoul <rinld0jsg@mozmail.com>
2025-10-03 12:25:53 +0100

    bugs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{Manager, Builder};
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
   let url = args.get(1).unwrap_or(&"https://copilot.microsoft.com/".to_string());
 
-  tauri::Builder::default()
+  Builder::default()
     .setup(move |app| {
       let window = app.get_window("main").unwrap();
       window.eval(&format!("window.location.replace('{}')", url)).unwrap();
