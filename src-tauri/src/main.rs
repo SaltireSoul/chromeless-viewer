6486244746916f34b731c0dffd8c63ab4e7aea90
SaltireSoul <rinld0jsg@mozmail.com>
2025-10-03 23:49:56 +0100

    03/10/2025 23:34:19.70

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,21 @@
 #![cfg_attr(windows, windows_subsystem = "windows")]
 
-use tauri::{Builder, Url};
+use tauri::Builder;
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
 
   let url = args.get(1).unwrap_or(&"https://copilot.microsoft.com/".to_string());
   let title = args.get(2).unwrap_or(&"Chromeless Viewer".to_string());
+
   let redirect_url = format!("index.html?url={}", url);
 
   Builder::default()
-    .with_url(Url::App(redirect_url.into()))
     .setup(move |app| {
-      let window = app.get_window("main").unwrap();
-      window.set_title(title).ok();
+      if let Some(window) = app.get_window("main") {
+        window.set_title(title).ok();
+        window.eval(&format!("window.location.replace('{}')", url)).ok();
+      }
       Ok(())
     })
     .run(tauri::generate_context!())
