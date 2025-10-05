785721065e0632a73d7bb356b8a8904b13fa5f00
SaltireSoul <rinld0jsg@mozmail.com>
2025-10-05 10:29:04 +0100

    double smeg

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(windows, windows_subsystem = "windows")]
 
 use tauri::{Builder, Manager};
 
@@ -6,10 +6,12 @@ fn main() {
   Builder::default()
     .setup(|app| {
       let window = app.get_webview_window("main").unwrap();
-      window.eval("window.location.replace('https://copilot.microsoft.com/')").unwrap();
+      window
+        .eval("window.location.replace('https://copilot.microsoft.com/')")
+        .unwrap();
       Ok(())
     })
     .build(tauri::generate_context!())
-    .expect("error while running tauri application")
-    .run();
+    .expect("error while building tauri application")
+    .run(|_app_handle, _event| {});
 }
